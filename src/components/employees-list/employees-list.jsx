3914111f6f03e9563630d57d010cc5e41ed3294f
finf0094 <finf0094@gmail.com>
2023-02-04 12:35:43 +0500

    ending layout

---
+++ src/components/employees-list/employees-list.jsx
@@ -0,0 +1,15 @@
+import EmployeesListItem from '../employees-list-item/employees-list-item'
+import './employers-list.css'
+
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+        </ul>
+    )
+}
+
+export default EmployeesList;
---
+++ src/components/search-panel/search-panel.jsx
@@ -0,0 +1,13 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <div>
+        <input type="text"
+               className="form-control search-input"
+               placeholder="Найти соотрудника"/>
+        </div>
+    )
+}
+
+export default SearchPanel
