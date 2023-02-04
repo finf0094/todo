51844ebf480c8e5da9781ed1f139f371bd5f0767
finf0094 <finf0094@gmail.com>
2023-02-04 18:49:18 +0500

    create data

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,11 +5,18 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-function App() {
 
+
+function App() {
+    const data = [
+        {name: "Askhat", salary: 800, increase: false},
+        {name: "Gulzhahan", salary: 109000, increase: true},
+        {name: "Kulush", salary: 2000, increase: false},
+    ]
 
     return (
         <div className="app">
+
             <AppInfo/>
 
             <div className="search-panel">
@@ -17,7 +24,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
 
         </div>
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -2,12 +2,18 @@ import EmployeesListItem from '../employees-list-item/employees-list-item'
 import './employers-list.css'
 
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+            <EmployeesListItem {...item}/>
+        )
+    });
+
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
