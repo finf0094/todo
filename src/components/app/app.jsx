45c99eab5c9eb58cb426beef105380df0d1f9ae7
finf0094 <finf0094@gmail.com>
2023-02-05 21:54:38 +0500

    renaming App.jsx functional to class component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,7 @@
+import { Component } from 'react';
+
 import './app.css'
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -7,32 +10,38 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 
 
-function App() {
-    const data = [
-        {name: "Askhat", salary: 800, increase: false, id: 1},
-        {name: "Gulzhahan", salary: 109000, increase: true, id: 2},
-        {name: "Kulush", salary: 2000, increase: false, id: 3},
-        {name: "Serikova", salary: 2000, increase: false, id: 4},
-    ]
+class App extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            data: [
+                {name: "Askhat", salary: 800, increase: false, id: 1},
+                {name: "Gulzhahan", salary: 109000, increase: true, id: 2},
+                {name: "Kulush", salary: 2000, increase: false, id: 3},
+                {name: "Serikova", salary: 2000, increase: false, id: 4},
+            ]
+        }
+    }
 
 
-
-    return (
-        <div className="app">
-            
-            <AppInfo/>
-
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    render() {
+        return (
+            <div className="app">
+                
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList data={this.state.data}
+                               onDelete={id => console.log(id)}/>
+                <EmployeesAddForm/>
+    
             </div>
-
-            <EmployeesList data={data}
-                           onDelete={id => console.log(id)}/>
-            <EmployeesAddForm/>
-
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
