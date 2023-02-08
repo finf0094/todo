e06330de08eb4d50e266e749a47b5b68d86f660b
finf0094 <finf0094@gmail.com>
2023-02-08 15:21:11 +0500

    adding filtration

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -20,7 +20,8 @@ class App extends Component {
                 {name: "Kulush", salary: 2000, increase: false, rise: false, id: 3},
                 {name: "Serikova", salary: 2000, increase: false, rise: false, id: 4},
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxId = 5;
     }
@@ -85,12 +86,27 @@ class App extends Component {
         this.setState({term: term})
     } 
 
+   
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise': 
+                return items.filter(item => item.rise)
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
 
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increasedemployees = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className="app">
@@ -102,7 +118,8 @@ class App extends Component {
                 <div className="search-panel">
                     <SearchPanel
                     onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter/>
+                    <AppFilter filter={filter}
+                               onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployeesList data={visibleData}
